b3ac9a28b8dad458f77b0b27426c7b4b4671ea76
jiyoung <jyyun423@gmail.com>
2020-08-11 13:29:26 +0900

    tmp

--- Matlab/utilities/colorspacetransform.m
+++ Matlab/utilities/colorspacetransform.m
@@ -1,5 +1,5 @@
 function [cstmat] = ...
-    colorspacetransform(target, balanced_camera_response, varargin)
+    colorspacetransform(balanced_camera_response, target, varargin)
 
 % Inputs:
 % balanced_camera_response: 24x3 white balacned camera's sensor responses
@@ -8,10 +8,8 @@ function [cstmat] = ...
 %
 % Optional Parameters:
 % loss:     (default = mse)
-% model:    remove the effects of the illumination
-%           'whitebalance' | 'fullcolorbalance' (default = whitebalance)
-% weight:   color space transform weight
-%
+% weights:   color space transform weight
+%            it should be 1x24 vector (default = [1, 1, ..., 1])
 % Outputs:
 % cbmat:    3x3 optimal color balance matrix
 % errs:     color differences on training sample specified by metric
@@ -21,27 +19,40 @@ param = parseInput(varargin{:});
 param = paramCheck(param);
 
 % check the inputs
-% add here
-% bias = camera_sensitivity * reflectance;
-% bias = bias.';
+N = size(balanced_camera_response, 1);
+assert(size(balanced_camera_response, 2) == 3,...
+       'Both test and target responses must be Nx3 matrices.');
+if ~isempty(param.weights)
+    assert(numel(param.weights) == N,...
+           'The number of weights does not match the number of test samples.');
+end
 
+% normalize the weights
+if ~isempty(param.weights)
+    param.weights = N * param.weights / sum(param.weights);
+else
+    param.weights = ones(N, 1);
+end
+
+% convert target responses to L*a*b* values
+target_lab = xyz2lab_(100*target); % for nonlinear opt
 
 % loss function handle
 lossfun = eval(['@', param.loss]);
 
+% print info
+paramPrint(param);
+
 % matrix calculation
 % set init matrix
-mat_r = lsqlin(camera_response, target(:,1));
-mat_g = lsqlin(camera_response, target(:,2));
-mat_b = lsqlin(camera_response, target(:,3));
-
-mat0 = [mat_r mat_g mat_b];
+mat0 = (balanced_camera_response' * diag(param.weights) * balanced_camera_response)^(-1) *...
+          balanced_camera_response' * diag(param.weights) * target;
 
 % mat0 = mat0.';
 
 switch lower(param.loss)
     case 'mse' % linear optimization
-        cbmat = mat0;
+        cstmat = mat0;
     otherwise
 end
 
@@ -52,7 +63,7 @@ function param = parseInput(varargin)
 % parse inputs & return structure of parameters
 parser = inputParser;
 parser.addParameter('loss', 'mse', @(x)ischar(x));
-parser.addParameter('model', 'whitebalance', @(x)ischar(x));
+parser.addParameter('weights', [], @(x)validateattributes(x, {'numeric'}, {'positive'}));
 parser.parse(varargin{:});
 param = parser.Results;
 end
@@ -60,13 +71,6 @@ end
 
 function param = paramCheck(param)
 % check the parameters
-
-% check the color balance model
-model_list = {'whitebalance', 'fullcolorbalance'};
-if ~ismember(lower(param.model), model_list)
-    error('%s is not a valid color balance model',...
-        param.model);
-end
 
 %check the loss function
 metric_list = {'mse'}; % add more
@@ -80,12 +84,17 @@ end
 function paramPrint(param)
 % make format pretty
 attr_idx = [2, 1];
+param.weights = sprintf('[%.2G, ..., %.2G]',...
+                        param.weights(1), param.weights(end));
 
-disp('Color balance training parameters:')
+disp('Color space transform training parameters:')
 disp('=================================================================');
 field_names = fieldnames(param);
 field_name_dict.loss = 'Loss function';
-field_name_dict.model = 'Color correction model';
+% field_name_dict.metric = 'Color difference metrics';
+% field_name_dict.model = 'Color correction model';
+% field_name_dict.observer = 'CIE standard colorimetric observer';
+field_name_dict.weights = 'Sample weights';
 
 for i = attr_idx
     len = fprintf('%s:',field_name_dict.(field_names{i}));
@@ -94,4 +103,3 @@ for i = attr_idx
 end
 disp('=================================================================');
 end
-
