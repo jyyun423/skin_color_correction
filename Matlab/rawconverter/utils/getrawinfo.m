7af7a8d371d55345d8405fd8f57c9dda86e59cb5
jiyoung <jyyun423@gmail.com>
2020-08-24 13:41:31 +0900

    working code-dngconverter

--- Matlab/rawconverter/utils/getrawinfo.m
+++
@@ -1,8 +0,0 @@
-function info = getrawinfo(raw_dir)
-% GETRAWINFO extracts image information such as exposure time, ISO speed,
-% F-number and so on via MATLAB built-in imfinfo function.
-
-info = imfinfo(raw_dir);
-if numel(info) > 1
-    info = info(1);
-end
