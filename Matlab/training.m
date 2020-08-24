5a2fc9548d9db50c6bdac952f29b9ed4db075efd
jyyun423 <37990748+jyyun423@users.noreply.github.com>
2020-08-24 13:31:34 +0900

    working code-training

--- Matlab/training.m
+++ Matlab/training.m
@@ -20,12 +20,9 @@ img = [];
 for i=1:length
     if ~dataset(i).isdir
         load (dataset(i).name)
-        patch_img = cat(3,patch_img,data);
+        patch_img = cat(3,patch_img,colors);
     end
 end
-
-% for m=1:length
-% end
 
 patch_rgb = getcolorpatch();
 patch_xyz = getcolorpatch('colorspace', 'xyz');
@@ -36,18 +33,20 @@ patch_lab = getcolorpatch('colorspace', 'lab');
 % choose the target color space; either srgb for display or xyz for
 % perceptual purpose
 
-% weight = ones(1,24) / 2;
+weight = ones(1,24) / 2;
 % weight(1) = 6.5;
 % weight(2) = 6.5;
-err = zeros(24,1);
+err = [];
 W_f = [];
+len = size(patch_img,3);
 
-for k=1:length
-    [W_f_tmp, err_tmp] = colorbalance(patch_img(k), patch_xyz,...
-        'model', 'fullcolorbalance', 'weights', weight);
-    fcbalanced_patch = patch_img(k) * W_f_tmp;
+for k=1:len
+    [W_f_tmp, err_tmp] = colorbalance(patch_img(:,:,k), patch_xyz,...
+        'model', 'fullcolorbalance', 'weights', weight,...
+        'loss', 'nonlinear', 'targetcolorspace', 'xyz');
+    fcbalanced_patch = patch_img(:,:,k) * W_f_tmp;
     W_f = cat(3, W_f, W_f_tmp);
-    err = err + err_tmp;
+    err = cat(2,err, err_tmp);
 end
 
 % err = err / length;
